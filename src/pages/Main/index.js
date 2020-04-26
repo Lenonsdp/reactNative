82f7a2de9b6597a1ee4ef967ac2f8179d080cac8
Lenon <lenon.paula@bling.com.br>
2020-04-26 20:36:19 -0300

    Estilizaço ã listagem de users e loading button

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,39 +1,41 @@
 import React, { Component } from 'react';
-import { Button, Keyboard } from 'react-native';
+import { Button, Keyboard, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import { Container, Form, Input, SubmitButton, List, User, Avatar, Name, Bio, ProfileButton, ProfileButtonText } from './styles';
 
 export default class Main extends Component {
     state = {
         newUser: '',
         users: [],
+        loading: false,
     };
 
     handleAddUser = async () => {
         const { users, newUser } = this.state;
-
+        this.setState({ loading: true });
         const response = await api.get(`/users/${newUser}`);
 
         const data = {
             name: response.data.name,
             login: response.data.login,
             bio: response.data.bio,
-            avatar: response.data.avatar,
+            avatar: response.data.avatar_url,
         };
 
         this.setState({
             users: [...users, data],
             newUser: '',
+            loading: false
         });
 
         Keyboard.dismiss();
     }
 
     render() {
-        const { users, newUser } = this.state;
+        const { users, newUser, loading } = this.state;
         return (
             <Container>
                 <Form>
@@ -46,10 +48,26 @@ export default class Main extends Component {
                         returnKeyType="send"
                         onSubmitEditing={this.handleAddUser}
                     />
-                    <SubmitButton onPress={this.handleAddUser}>
-                        <Icon name="add" size={20} color="#FFF" />
+                    <SubmitButton loading={loading} onPress={this.handleAddUser}>
+                        { loading ? <ActivityIndicator color='#FFF' /> : <Icon name="add" size={20} color="#FFF" />}
                     </SubmitButton>
                 </Form>
+
+                <List
+                    data={users}
+                    keyExtractor={user => user.login}
+                    renderItem={({ item }) => (
+                        <User>
+                            <Avatar source={{ uri: item.avatar }} />
+                            <Name>{item.name}</Name>
+                            <Bio>{item.bio}</Bio>
+
+                            <ProfileButton onPress={() => {}}>
+                                <ProfileButtonText>Ver Perfil</ProfileButtonText>
+                            </ProfileButton>
+                        </User>
+                    )}
+                />
                 {/* <Button title='Go to User' onPress={() => navigation.navigate('User')}/> */}
             </Container>
         );
