b1acc4a1980fbc122e26d3fbc69b25978c794590
Lenon <lenon.paula@bling.com.br>
2020-04-26 19:08:39 -0300

    Adicionado axios e configurada classe main para obter users;

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import { Button } from 'react-native';
+import React, { Component } from 'react';
+import { Button, Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
+
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main({ navigation }) {
-    return (
-        <Container>
-            <Form>
-                <Input
-                    autoCorrect={false}
-                    autoCapitalize="none"
-                    placeholder="Adicionar usuário"
-                />
-                <SubmitButton>
-                    <Icon name="add" size={20} color="#fff" />
-                </SubmitButton>
-            </Form>
-            {/* <Button title='Go to User' onPress={() => navigation.navigate('User')}/> */}
-        </Container>
-    );
+export default class Main extends Component {
+    state = {
+        newUser: '',
+        users: [],
+    };
+
+    handleAddUser = async () => {
+        const { users, newUser } = this.state;
+
+        const response = await api.get(`/users/${newUser}`);
+
+        const data = {
+            name: response.data.name,
+            login: response.data.login,
+            bio: response.data.bio,
+            avatar: response.data.avatar,
+        };
+
+        this.setState({
+            users: [...users, data],
+            newUser: '',
+        });
+
+        Keyboard.dismiss();
+    }
+
+    render() {
+        const { users, newUser } = this.state;
+        return (
+            <Container>
+                <Form>
+                    <Input
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Adicionar usuário"
+                        value={newUser}
+                        onChangeText={ text => this.setState({ newUser: text })}
+                        returnKeyType="send"
+                        onSubmitEditing={this.handleAddUser}
+                    />
+                    <SubmitButton onPress={this.handleAddUser}>
+                        <Icon name="add" size={20} color="#FFF" />
+                    </SubmitButton>
+                </Form>
+                {/* <Button title='Go to User' onPress={() => navigation.navigate('User')}/> */}
+            </Container>
+        );
+    }
 }
