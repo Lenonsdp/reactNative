5183815f95f80415f348eca87323343ffadb43ad
Lenon <lenon.paula@bling.com.br>
2020-04-26 20:55:06 -0300

    Utilização do AsyncStorage mobile para armazenamento

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button, Keyboard, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
+import AsyncStorage from '@react-native-community/async-storage';
 
 
 import { Container, Form, Input, SubmitButton, List, User, Avatar, Name, Bio, ProfileButton, ProfileButtonText } from './styles';
@@ -12,6 +13,22 @@ export default class Main extends Component {
         users: [],
         loading: false,
     };
+
+    async componentDidMount() {
+        const users = await AsyncStorage.getItem('users');
+
+        if (users) {
+            this.setState({ users: JSON.parse(users) });
+        }
+    }
+
+    async componentDidUpdate(_, prevState) {
+        const { users } = this.state;
+
+        if(prevState.users != this.state.users){
+            await AsyncStorage.setItem('users', JSON.stringify(users));
+        }
+    }
 
     handleAddUser = async () => {
         const { users, newUser } = this.state;
